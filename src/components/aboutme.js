e87629145046b45108704b428bbe844d4f651cb5
kwinkler99 <kasia.winkler99@gmail.com>
2021-08-17 21:49:00 +0200

    add animation to page abput me

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -1,11 +1,20 @@
 import React from 'react'
 import '../style/about-me.scss'
 
+
+
 function AboutMe() {
+
+    const stars = []
+    for(let i = 0; i < 20; i++){
+        stars.push(<div className="star" id={i} key={i}></div>)
+    }
+    
     return (
         <div id="about-me">
-            <div className="stars">
+            <div className="cosmos">
                 <h1>Hello</h1>
+                {stars}
             </div>
         </div>
     );
