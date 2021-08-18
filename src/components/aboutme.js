60b312f65253ce9d47ac9d1299830cb1fa981524
kwinkler99 <kasia.winkler99@gmail.com>
2021-08-18 18:20:58 +0200

    adding animation to page about me(cosmos, falling stars and planets with shadow)

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -6,8 +6,8 @@ import '../style/about-me.scss'
 function AboutMe() {
 
     const stars = []
-    for(let i = 0; i < 20; i++){
-        stars.push(<div className="star" id={i} key={i}></div>)
+    for(let i = 0; i < 50; i++){
+        stars.push(<span className="star" id={i} key={i}></span>)
     }
     
     return (
@@ -15,6 +15,18 @@ function AboutMe() {
             <div className="cosmos">
                 <h1>Hello</h1>
                 {stars}
+                <div className="planets">
+                    <span className="sun"></span>
+                    <span className="mercury"></span>
+                    <span className="venus"></span>
+                    <span className="earth"></span>
+                    <span className="mars"></span>
+                    <span className="jupiter"></span>
+                    <span className="saturn"></span>
+                    <span className="uranus"></span>
+                    <span className="neptune"></span>
+                    <span className="pluto"></span>
+                </div>
             </div>
         </div>
     );
